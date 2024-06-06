416ca9d93bcce3762fdb85a85e0a3644b7b57cd2
Abuzar akhtar <abuzar0013@gmail.com>
2024-06-06 19:14:06 +0530

    number updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-container">
         <img src={logo} alt="Logo" className="footer-logo" />
         <a href="mailto:contact@bakra.live" className="footer-email">contact@bakra.live</a>
-        <p className="footer-catchy-line">Celebrate Eid-ul-Adha with the Best, Find Your Perfect Qurbani Goat Today!</p>
+        <p className="footer-catchy-line">Get your premium goat without visiting the market – free home delivery!</p>
         <p className="footer-text">© 2024 Bakra.live. All rights reserved.</p>
       </div>
     </div>
